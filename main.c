caa70d6062b5bdcc0197a02c00616518ec5289ff
danielkuchler <danielkuchler@mwc-033233.dhcp.missouri.edu>
2016-11-03 16:00:41 -0500

    brian sucks

--- main.c
+++ main.c
@@ -7,11 +7,7 @@ int main (void) {
   printf("Holt Skinner\n");
   //Add your printf below here!
   printf("Shelby Heffron\n");
-<<<<<<< HEAD
-  printf("Danny Kuchler")
-
-=======
   printf("Ryan Filkins\n");
->>>>>>> origin/master
+
   return 0;
 }
