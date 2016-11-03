94af844076f1adb1c9a6a90bfdf0609ec71e5197
Derek R <derek13f6@gmail.com>
2016-11-03 15:57:09 -0500

    Updated the *better* version of the repo

--- main.c
+++ main.c
@@ -5,13 +5,20 @@
 int main (void) {
 
   printf("Holt Skinner\n");
+  /*
+  
+  I'll make a better branch!
+  One with blackjack and hookers!
+  
+  */
 
   //Add your printf below here!
   printf("Jason Pae\n");
   printf("Shelby Heffron\n");
   printf("Ryan Filkins\n");
 
-  printf("Reis Rosenbloom\n")
+  printf("Derek Reed\n");
+  printf("Reis Rosenbloom\n");
 
   return 0;
 }
