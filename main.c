96c3ec7f1b1f35dca325ad27fcaf6587895387d5
Ethan Schutzenhofer <ews624@mail.missouri.edu>
2016-11-03 15:58:22 -0500

    Revert "Changed comment"
    This reverts commit 677501aca81769e8eeaf69a5da494f5c47e5115e.
    
    # Conflicts:
    #	main.c

--- main.c
+++ main.c
@@ -5,9 +5,13 @@
 int main (void) {
 
   printf("Holt Skinner\n");
+<<<<<<< HEAD
   printf("Ryan Filkins\n");
   //Add your printf below here!
   printf("Shelby Heffron\n");
+=======
+  //Add your printf here!!!
+>>>>>>> parent of 677501a... Changed comment
 
   return 0;
 }
